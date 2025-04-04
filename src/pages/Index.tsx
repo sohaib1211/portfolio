394db1ccfeeace0cc5128ee689b6d19d88f79849
sohaib1211 <sohaibmohammad46@gmail.com>
2025-04-04 23:55:49 +0500

    adding all files

---
+++ src/pages/Index.tsx
@@ -0,0 +1,28 @@
+
+import Navbar from '@/components/Navbar';
+import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
+import ProjectsSection from '@/components/ProjectsSection';
+import SkillsSection from '@/components/SkillsSection';
+import ExperienceSection from '@/components/ExperienceSection';
+import FutureSection from '@/components/FutureSection';
+import ContactSection from '@/components/ContactSection';
+import Footer from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <ExperienceSection />
+      <FutureSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
